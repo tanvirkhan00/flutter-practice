121a29106e02676a7aeb917f6276b442737a6160
Tanvir Khan <kmtanvir1111@gmail.com>
2025-07-14 15:28:21 +0600

    practice new widget

--- lib/class3.dart
+++ lib/class3.dart
@@ -6,7 +6,7 @@ class Class3  extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.grey.shade500,
+      // backgroundColor: Colors.grey.shade500,
       appBar: AppBar(
         title: Text('App Bar', style: TextStyle(color:Colors.white, fontSize: 30,fontWeight: FontWeight.w600)),
         centerTitle: true,
@@ -25,7 +25,55 @@ class Class3  extends StatelessWidget {
               ),
                 onPressed:(){
               print('Clicked');
-            }, child: Text('Submit'))
+            }, child: Text('Submit')),
+            SizedBox(
+              height: 20,
+            ),
+            SizedBox(
+              // width: 300,
+              width: double.infinity,
+              height: 60,
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.yellow,
+                  foregroundColor: Colors.black,
+                  textStyle: TextStyle(fontSize: 25,)
+                ),
+                  onPressed:(){
+                print('Clicked');
+              }, child: Text('Submit')),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            OutlinedButton(onPressed: (){}, child: Text('Outline Button')),
+            SizedBox(
+              height: 20,
+            ),
+            TextButton(onPressed: (){}, child: Text('Text Button', style: TextStyle(color: Colors.blue, fontSize: 30, fontWeight: FontWeight.w700),)),
+            Icon(Icons.phone_bluetooth_speaker,
+              size: 50,
+            ),
+            IconButton(onPressed: (){
+              print("Object Deleted");
+            }, icon: Icon(Icons.delete, size: 50,color: Colors.red,)),
+            SizedBox(
+              height: 20,
+            ),
+            GestureDetector(
+              onTap: (){
+                print("Test Press");
+              },
+                child: Text("Read More", style: TextStyle(fontSize: 30),)),
+            InkWell(
+              onLongPress: (){
+                print("Load Content.............");
+              } ,
+                child: Text("More Content", style: TextStyle(fontSize: 30, color: Colors.red),)),
+            Padding(
+                 // padding: EdgeInsets.only(top: 40, right: 50),
+              padding: EdgeInsets.symmetric(vertical: 30, horizontal: 50),
+                child: Text("This is padding", style: TextStyle(fontSize: 30, color: Colors.red),))
           ],
         ),
       ),
