4ba12a2671e4bfec9a29e1c6a451f5f996aac99d
Tanvir Khan <kmtanvir1111@gmail.com>
2025-08-06 19:27:17 +0600

    complete_counter_app

--- lib/Module_11/counterApp.dart
+++ lib/Module_11/counterApp.dart
@@ -1,32 +1,56 @@
 import 'package:flutter/material.dart';
 
-class CounterApp extends StatelessWidget {
-  const CounterApp({super.key});
+class CounterApp extends StatefulWidget {
+   CounterApp({super.key});
+
+  @override
+  State<CounterApp> createState() => _CounterAppState();
+}
+
+class _CounterAppState extends State<CounterApp> {
+  int number =0;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Counter App'),
+        title: Text('Counter App',style: TextStyle(
+          fontSize: 30,
+          fontWeight: FontWeight.w500
+        ),),
       ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text('0', style: TextStyle(
-              fontSize: 50
+            Text(number.toString(),
+              style: TextStyle(
+              fontSize: 60
             ),),
+            SizedBox(
+              height: 20,
+            ),
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: [
-                ElevatedButton(onPressed: (){},
+                ElevatedButton(onPressed: (){
+                  setState(() {
+                    number -- ;
+                  });
+                  print(number);
+                },
                     style: ElevatedButton.styleFrom(
                       backgroundColor: Colors.yellow
                     ),
                     child: Text('-', style: TextStyle(
                   fontSize: 30
                 ),)),
-                ElevatedButton(onPressed: (){},
+                ElevatedButton(onPressed: (){
+                  setState(() {
+                    number ++;
+                  });
+                  print(number);
+                },
                     style: ElevatedButton.styleFrom(
                       backgroundColor: Colors.yellow
                     ),
