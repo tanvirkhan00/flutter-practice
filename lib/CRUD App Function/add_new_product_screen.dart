9b4c441c3658087e552740afbdaa5950b184b69e
Tanvir Khan <kmtanvir1111@gmail.com>
2025-08-27 21:12:22 +0600

    practice CRUD Operation Add Product

--- lib/CRUD App Function/add_new_product_screen.dart
+++ lib/CRUD App Function/add_new_product_screen.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart';
 
 class AddNewProductScreen extends StatefulWidget {
   const AddNewProductScreen({super.key});
@@ -8,6 +11,7 @@ class AddNewProductScreen extends StatefulWidget {
 }
 
 class _AddNewProductScreenState extends State<AddNewProductScreen> {
+  bool _addProductInProgress = false ;
   final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
   final TextEditingController _nameTEController = TextEditingController();
   final TextEditingController _codeTEController = TextEditingController();
@@ -36,6 +40,13 @@ class _AddNewProductScreenState extends State<AddNewProductScreen> {
                       hintText: 'Product Name',
                       labelText: 'Product Name'
                     ),
+                    validator: (String? value){
+                      if(value?.trim().isEmpty ?? true){
+                        return("Enter Your Value");
+                      } else {
+                        return null ;
+                      }
+                    },
                   ),
                   TextFormField(
                     controller: _codeTEController,
@@ -44,6 +55,13 @@ class _AddNewProductScreenState extends State<AddNewProductScreen> {
                       hintText: 'Product Code',
                       labelText: 'Product Code'
                     ),
+                    validator: (String? value){
+                      if(value?.trim().isEmpty ?? true){
+                        return("Enter Your Value");
+                      } else {
+                        return null ;
+                      }
+                    },
                   ),
                   TextFormField(
                     controller: _quantityTEController,
@@ -53,6 +71,13 @@ class _AddNewProductScreenState extends State<AddNewProductScreen> {
                       hintText: 'Quantity',
                       labelText: 'Quantity'
                     ),
+                    validator: (String? value){
+                      if(value?.trim().isEmpty ?? true){
+                        return("Enter Your Value");
+                      } else {
+                        return null ;
+                      }
+                    },
                   ),
                   TextFormField(
                     controller: _priceTEController,
@@ -62,6 +87,13 @@ class _AddNewProductScreenState extends State<AddNewProductScreen> {
                       hintText: 'Unit Price',
                       labelText: 'Unit Price'
                     ),
+                    validator: (String? value){
+                      if(value?.trim().isEmpty ?? true){
+                        return("Enter Your Value");
+                      } else {
+                        return null ;
+                      }
+                    },
                   ),
                   TextFormField(
                     controller: _imageTEController,
@@ -69,16 +101,83 @@ class _AddNewProductScreenState extends State<AddNewProductScreen> {
                       hintText: 'Image Url',
                       labelText: 'Image Url'
                     ),
+                    validator: (String? value){
+                      if(value?.trim().isEmpty ?? true){
+                        return("Enter Your Value");
+                      } else {
+                        return null ;
+                      }
+                    },
                   ),
                   SizedBox(height: 10,),
-                  FilledButton(onPressed: (){},
-                      child: Text("Add Product"))
+                  Visibility(
+                    visible: _addProductInProgress == false,
+                    replacement: Center(
+                        child: CircularProgressIndicator()),
+                    child: FilledButton(onPressed: _addNewProduct,
+                        child: Text("Add Product")),
+                  )
                 ],
               ),
             )),
       ),
     );
   }
+
+  Future<void> _addNewProduct () async {
+    if(_formKey.currentState!.validate()) {
+      return;
+    }
+
+    _addProductInProgress = true ;
+    setState(() { });
+    Uri uri = Uri.parse('http://35.73.30.144:2008/api/v1/CreateProduct');
+
+     int totalPrice = int.parse(_priceTEController.text) * int.parse(_quantityTEController.text);
+      Map<String, dynamic> requestedBody ={
+        "ProductName": _nameTEController.text,
+        "ProductCode": int.parse(_codeTEController.text),
+        "Img": _imageTEController.text,
+        "Qty": int.parse(_quantityTEController.text),
+        "UnitPrice": int.parse(_priceTEController.text),
+        "TotalPrice": totalPrice
+    };
+      //Request data
+    Response response = await post(uri ,
+        headers: {
+         'Content-type': 'application/json'
+        },
+        body: jsonEncode(requestedBody));
+    print(response.statusCode);
+    print(response.body);
+
+    if(response.statusCode == 200){
+       final decodedJson = jsonDecode(response.body);
+       if(decodedJson['status'] == 'success') {
+         clearTextFeild();
+         ScaffoldMessenger.of(context).showSnackBar(
+             SnackBar(content: Text('Product Created Successfully')));
+    } else {
+         String errorMessage = decodedJson['data'];
+         ScaffoldMessenger.of(context).showSnackBar(
+             SnackBar(content: Text(errorMessage)));
+       }
+    }
+
+    _addProductInProgress = false ;
+    setState(() { });
+  }
+
+
+
+  void clearTextFeild(){
+    _nameTEController.clear();
+    _codeTEController.clear();
+    _quantityTEController.clear();
+    _priceTEController.clear();
+    _imageTEController.clear();
+  }
+
   @override
   void dispose() {
     _nameTEController.dispose();
